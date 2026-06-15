a18b6cae2d41400ccfd6bf4c1cfb82ec887de423
teswinT <teswin007@gmail.com>
2026-06-16 00:11:01 +0530

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -3,17 +3,27 @@
 class Solution():
     def merge(self,arr,low,mid,high):
         temp = []
-        left_pointer = low #pointer of the left part
-        right_pointer = mid+1 # pointer of the right part
+        left,right = low,mid+1 # left is leftpointer and right is rightpointer
 
-        
-        while left_pointer <= mid and right_pointer <= high:
-            if left_pointer < right_pointer:
-                temp.append(arr[left_pointer])
-                left_pointer += 1
+        while left <= mid and right <= high:
+            if arr[left] <= arr[right]:
+                temp.append(arr[left])
+                left += 1
             else:
-                temp.append(arr[right_pointer])
-                right_pointer += 1
+                temp.append(arr[right])
+                right += 1
+        
+        while left <= mid:
+            temp.append(arr[left])
+            left += 1
+        
+        while right <= high:
+            temp.append(arr[right])
+            right += 1
+        
+        for i in range(low, high + 1):
+            arr[i] = temp[i - low]
+        
 
         
 
@@ -22,9 +32,17 @@ class Solution():
 
         if low >= high: # base case
             return None
-        
         mid = (low + high)//2
 
         self.mergesort(arr, low, mid)
         self.mergesort(arr, mid+1, high)
         self.merge(arr, low, mid, high)
+
+
+arr = [2,3,1,4,5]
+print(arr)
+
+sol = Solution()
+sol.mergesort(arr,0,len(arr)-1)
+
+print(arr)
